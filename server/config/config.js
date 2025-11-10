94041379be2da0e91e0e81dff25847042756ae95
Muhammad Ahsan Ehsan <ahsanehsan00@gmail.com>
2025-11-10 16:45:31 +0500

    chore: remove config.js from .gitignore and add initial config file

---
+++ server/config/config.js
@@ -0,0 +1,6 @@
+// import dotenv from 'dotenv';
+
+// dotenv.config({ path: '.env' });
+
+// export const DATABASE_LOCAL = process.env.DATABASE_LOCAL;
+// export const DATABASE_CLOUD = process.env.DATABASE_CLOUD;
